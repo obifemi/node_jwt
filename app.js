bbf6b0bcb818fdadd533c676438f13be3c858cb1
ali mert inal <mert.inal@gmail.com>
2023-05-31 22:32:20 +0200

    add port env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
 const app = express();
-const port = 3000;
+const port = process.env.PORT
 
 
 dotenv.config();
